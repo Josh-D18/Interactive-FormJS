bc5d38ce03c9abe43c873ba9d8c07bbc47377520
Josh_Date <jjdate@live.com>
2020-12-27 14:36:18 -0500

    Working on Activities Section

--- js/script.js
+++ js/script.js
@@ -13,7 +13,8 @@ function jobRoleFunc(){
     let otherJobRole = document.querySelector('#other-job-role');
 
     otherJobRole.style.display = 'none';
-    
+
+    // Event Listener for the Job Roles. This will check if value if equal to 'other' and change the display value
     jobRoles.addEventListener('click', (e) => {
         console.log(e.target.value)
         if (e.target.value === 'other'){
@@ -41,10 +42,7 @@ function colorTShirtFunc(){
         let heartShirt = document.querySelectorAll('[data-theme="heart js"]');
         let punShirt = document.querySelectorAll('[data-theme="js puns"]');
 
-
-        console.log(heartShirt);
-        console.log(shirtStyle);
-
+        // If statement to check if the shirt style is 'js puns' or 'heart js'
         if (shirtStyle === 'js puns'){
             for (let i = 0; i < heartShirt.length; i++){
                 heartShirt[i].style.display = 'none';
@@ -65,12 +63,35 @@ colorTShirtFunc();
 // Register for Activites section
 
 function activitiesFunc(){
-    let activities = document.querySelector('#activities-box'); 
-    
+    let activitiesButtons = document.querySelectorAll('.activities label input'); 
+    let activities = document.querySelector("#activities");
     let totalCost = document.querySelector('#activities-cost');
 
-    activities.addEventListener('click', (e)=>{
-        console.log(e.target.value);
+     // Number to track amount added to Total
+        let totalAmount = 0;
+
+    activities.addEventListener('change', (e)=>{
+        let cost = document.querySelector('#activities-cost');
+        let clicked = e.target;
+        let clickedCost = parseInt(e.target.getAttribute('data-cost'));
+
+
+        // Loop to Check If Button Clicked Matches
+        for (let i = 0;i < activitiesButtons.length; i++){
+            
+            let money = parseInt(activitiesButtons[i].getAttribute('data-cost'));
+
+            if (clickedCost === money && clicked !== activitiesButtons[i]){
+                if (clicked.checked){
+                    totalAmount = totalAmount += money;
+                    console.log(totalAmount);
+                } else {
+                    totalAmount = money -= totalAmount;
+                    console.log(totalAmount);
+                }
+            }
+        }
+        cost.textContent = `Total: $${totalAmount}`;
     });
 }
 
