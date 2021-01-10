e288cb42002241b6b16dfb213d08d449d4e3f130
Josh_Date <jjdate@live.com>
2021-01-10 13:42:54 -0500

    Working on Form Section

--- js/script.js
+++ js/script.js
@@ -62,37 +62,173 @@ colorTShirtFunc();
 
 // Register for Activites section
 
-function activitiesFunc(){
-    let activitiesButtons = document.querySelectorAll('.activities label input'); 
-    let activities = document.querySelector("#activities");
-    let totalCost = document.querySelector('#activities-cost');
-
-     // Number to track amount added to Total
+    // Number to track amount added to Total
         let totalAmount = 0;
+
+function activitiesFunc(){
+    let activities = document.querySelector("#activities");
+
+
 
     activities.addEventListener('change', (e)=>{
         let cost = document.querySelector('#activities-cost');
         let clicked = e.target;
         let clickedCost = parseInt(e.target.getAttribute('data-cost'));
 
-
-        // Loop to Check If Button Clicked Matches
-        for (let i = 0;i < activitiesButtons.length; i++){
-            
-            let money = parseInt(activitiesButtons[i].getAttribute('data-cost'));
-
-            if (clickedCost === money && clicked !== activitiesButtons[i]){
-                if (clicked.checked){
-                    totalAmount = totalAmount += money;
-                    console.log(totalAmount);
-                } else {
-                    totalAmount = money -= totalAmount;
-                    console.log(totalAmount);
-                }
-            }
+        if (clicked.checked){
+            totalAmount += clickedCost;
+        } else {
+            totalAmount -= clickedCost;
         }
         cost.textContent = `Total: $${totalAmount}`;
     });
 }
 
 activitiesFunc();
+
+
+// Payment Info section
+
+function paymentFunc(){
+    let paymentMethod = document.querySelector('#payment');
+
+    // Payment Box
+    let paymentBox = document.querySelector('.credit-card-box');
+    let credit = document.querySelector('.expiration-box');
+
+    // Bitcoin
+    let bitCoin = document.querySelector('#bitcoin')
+
+    // Paypal
+    let payPal = document.querySelector('#paypal');
+    
+    // Hiding both bitcoin and paypal options and keeping credit card options open
+    
+    bitCoin.hidden = true;
+    payPal.hidden = true;
+    credit.hidden = false;
+    paymentBox.hidden = false;
+
+    paymentMethod.addEventListener('change', (e)=>{
+        let number = e.target.options.selectedIndex;
+        let payment = paymentMethod.options[number].innerHTML;
+        
+        // If statements for which payment the user chooses. The one picked will show up on screen and the others will stay hidden
+
+        if (payment === 'PayPal'){
+            credit.style.display = 'none';
+            paymentBox.style.display = 'none';
+            bitCoin.hidden = true;
+            payPal.hidden = false;
+        } 
+
+        if (payment === 'Bitcoin'){
+            credit.style.display = 'none';
+            paymentBox.style.display = 'none';
+            payPal.hidden = true;
+            bitCoin.hidden = false;
+        }
+
+        if (payment === 'Credit Card'){
+            credit.style.display = '';
+            paymentBox.style.display = '';
+            payPal.hidden = true;
+            bitCoin.hidden = true;
+        }
+    });
+
+}
+
+paymentFunc();
+
+
+// Form Validation 
+
+// Hints
+let nameHint = document.querySelector('#name-hint');
+let emailHint = document.querySelector("#email-hint");
+let activitiesHint = document.querySelector('#activities-hint');
+
+// -- Name Helper Function
+function nameIsValid(){
+    let name = document.querySelector('#name');
+    let nameHint = document.querySelector('#name-hint');
+
+    if (name.value === '' || name.length < 0){
+        return false;
+    } else{
+        return true;
+    }
+}
+
+// -- Email Helper Function
+function isEmailValid(){
+    let emailReg = /^[^@]+@[^@.]+\.[a-z]+$/i;
+    let emailValue = document.querySelector('#email').value;
+
+    if (emailReg.test(emailValue)){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+
+// Register Activity Helper Function
+
+function activityChecker(){
+    let activities = document.querySelectorAll("#activities-box label input");
+    
+    for (let i = 0; i < activities.length; i++){
+        if (activities[i].checked){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+// Credit Card Helper Function
+
+function creditCard(){
+    let paymentMethodCreditCard = document.querySelector('#payment').options[1];
+    
+    // The "Card number" field must contain a 13 - 16 digit credit card number with no dashes or spaces. The value does not need to be a real credit card number.
+// The "Zip code" field must contain a 5 digit number.
+// The "CVV" field must contain a 3 digit number.
+
+
+    if (paymentMethodCreditCard.selected){
+
+    }
+}
+
+creditCard();
+
+
+
+
+function formValidate(){
+    let form = document.querySelector('form');
+    
+    form.addEventListener('submit', (e)=>{
+        
+        if (!nameIsValid()){
+            e.preventDefault();
+            nameHint.classList.remove('hint');
+        }
+
+        if (!isEmailValid()){
+            e.preventDefault();
+            emailHint.classList.remove('hint');
+        }
+
+        if (!activityChecker()){
+            e.preventDefault();
+            activitiesHint.classList.remove('hint');
+        }
+    });
+}
+
+formValidate();
